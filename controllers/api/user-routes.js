37f197092f86d11b2a8ab22c8aa1d5e855d003af
lisahuele <lisahuele@gmail.com>
2021-09-04 19:39:27 -0400

    installed dependencies and created folders

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -66,11 +66,11 @@ router.post('/', (req, res) => {
 
             res.json(dbUserData);
         })
-    });
-})
-.catch(err => {
-    console.log(err);
-    res.status(500).json(err);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 //log in
